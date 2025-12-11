774c9278d6fe837f428e5d9660d3b52969a9f4cd
manudj195 <81382442+manudj195@users.noreply.github.com>
2025-12-11 11:02:50 +0000

    add bookmarlet

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
+const path = require('path');
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -13,9 +15,32 @@ const puppeteer = require('puppeteer');
   
   const page = await browser.newPage();
   
-  // I tuoi URL preferiti
-  await page.goto('https://www.immobiliare.it/agenzie-immobiliari/438027/maison-prati-srl/');
-  console.log('Aperto Google!');
+  // Configura download
+  const downloadPath = path.resolve('./downloads');
+  if (!fs.existsSync(downloadPath)) {
+    fs.mkdirSync(downloadPath);
+  }
   
+  const client = await page.createCDPSession();
+  await client.send('Page.setDownloadBehavior', {
+    behavior: 'allow',
+    downloadPath: downloadPath
+  });
+  
+  // Carica lo scraper dal file esterno
+  const scraperCode = fs.readFileSync('./scraper.js', 'utf8');
+  
+  // Apri il sito
+  await page.goto('https://www.immobiliare.it/agenzie-immobiliari/438027/maison-prati-srl/', { waitUntil: 'networkidle2' });
+  console.log('Pagina caricata!');
+  
+  // Esegui lo scraper
+  await page.evaluate(scraperCode);
+  console.log('Scraping avviato!');
+  
+  // Attendi completamento
+  await page.waitForTimeout(600000);
+  
+  console.log('Scraping completato!');
   await browser.close();
 })();
