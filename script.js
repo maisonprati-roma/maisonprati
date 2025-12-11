59e5824657100c340e196006e0edb3fe279150df
manudj195 <81382442+manudj195@users.noreply.github.com>
2025-12-11 10:53:47 +0000

    update

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-gpu'
+    ]
+  });
+  
   const page = await browser.newPage();
   
   // I tuoi URL preferiti
   await page.goto('https://www.immobiliare.it/agenzie-immobiliari/438027/maison-prati-srl/');
-  console.log('Aperto !');
+  console.log('Aperto Google!');
   
   await browser.close();
 })();
